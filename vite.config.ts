4bba9910168ad9a9b7e24ae77179eb381489622c
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2024-12-04 01:23:15 +0530

    Removing unwanted configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from "vite-plugin-svgr";
-import dts from "vite-plugin-dts";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svgr(), dts(), react()],
-  build: {
-    outDir: "build",
-  },
-})
+  plugins: [react()],
+});
